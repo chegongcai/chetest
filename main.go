cba60255aa827525b215d45c381a6a19a9a2113a
chegongcai <chj040432@sina.com>
2018-07-18 17:27:15 +0800

    parse

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	service := ":8080"
+	//testbuf()
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
 	checkErr(err)
 	listener, err := net.ListenTCP("tcp", tcpAddr)
@@ -32,6 +33,7 @@ func checkErr(err error) {
 
 func handleClient(conn net.Conn) {
 	defer conn.Close()
+	var rev_buf *string
 	var buf [512]byte
 	for {
 		n, err := conn.Read(buf[0:])
@@ -41,9 +43,8 @@ func handleClient(conn net.Conn) {
 		rAddr := conn.RemoteAddr()
 		//fmt.Println("Receive from client", rAddr.String(), string(buf[0:n]))
 		fmt.Println("client IP", rAddr.String())
-		rev_buf := string(buf[0:n])
-		str_command := string(buf[0:5])
-		ParseProtocol(rev_buf, str_command, conn) //do protocol parse
+		*rev_buf = string(buf[0:n])
+		ParseProtocol(rev_buf, conn) //do protocol parse
 	}
 }
 
@@ -58,10 +59,58 @@ func GetZone() string {
 	buf := []byte(local_str)
 	return string(buf[32:33])
 }
-func ParseProtocol(rev_buf string, command string, conn net.Conn) {
+
+func GetAsciiStrFromBuffer(StrOut1 *string, buf *string, strMaxlen int, StrIn *string) int {
+	i := 0
+
+	str := []byte(*StrIn)
+	var strBuf [512]byte
+
+	fmt.Println(string(str[0:]))
+
+	for i = 0; (str[i] != ',') && (str[i] != '#'); i++ {
+		strBuf[i] = str[i]
+
+		if i >= strMaxlen {
+			return 0
+		}
+	}
+	i++
+	*StrOut1 = string(strBuf[0:i])
+	*buf = string(str[i:])
+	return i
+}
+
+func testbuf() {
+	var command, test, buf, imei, zone *string
+
+	test = new(string)
+	command = new(string)
+	buf = new(string)
+	imei = new(string)
+	zone = new(string)
+
+	*test = "BDS01,20180718143213,8#"
+
+	GetAsciiStrFromBuffer(command, buf, 6, test)
+	fmt.Println("get command:", *command)
+
+	GetAsciiStrFromBuffer(imei, buf, 15, buf)
+	fmt.Println("get imei:", *imei)
+
+	GetAsciiStrFromBuffer(zone, buf, 1, buf)
+	fmt.Println("get zone:", *zone)
+}
+
+func ParseProtocol(rev_buf *string, conn net.Conn) {
 	var err error
-	fmt.Println("Receive from client", rev_buf)
-	switch command {
+	var command, buf *string
+	fmt.Println("Receive from client", *rev_buf)
+
+	GetAsciiStrFromBuffer(command, buf, 6, rev_buf)
+	fmt.Println("get command:", *command)
+
+	switch *command {
 	case "BDT01":
 		zone, _ := strconv.Atoi(GetZone())
 		buf := fmt.Sprintf("BDS01,%s,%d#", GetTimeStamp(), zone)
