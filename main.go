ea594d32ca84a26d8635b6960332d52518383862
chegongcai <chj040432@sina.com>
2018-07-19 14:59:56 +0800

    parse

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -45,14 +44,12 @@ func handleClient(conn net.Conn) {
 			return
 		}
 		rAddr := conn.RemoteAddr()
-		fmt.Println("client IP", rAddr.String(), string(buf[0:n]))
-		/*
-			if buf[n-1] != '#' {
-				return
-			}
-			rev_buf := string(buf[0 : n-1]) //delete the tail #
-			ParseProtocol(rev_buf, conn)    //do protocol parse
-		*/
+		fmt.Println("client IP", rAddr.String())
+		if buf[n-1] != '$' {
+			return
+		}
+		rev_buf := string(buf[0 : n-1]) //delete the tail #
+		ParseProtocol(rev_buf, conn)    //do protocol parse
 	}
 }
 
@@ -86,55 +83,48 @@ func ParseStatusData(str string) (signal string, sat_num string, bat string, mod
 
 func testbuf() {
 
-	var temp []string
-	var flag string = "BDT03,7,460,0,9520|3671|13,9520|3672|12,9520|3673|11,9520|3674|10,9520|3675|9,9520|3676|8,9520|3677|7"
-	temp = strings.Split(flag, ",")
+	var arr_buf, data_buf, comand_buf []string
 
-	lbs_num, _ := strconv.Atoi(string(temp[1]))
-	lbs_mnc := string(temp[2])
-	lbs_mcc := string(temp[3])
-	for i := 4; i < lbs_num+4; i++ {
-		fmt.Println(temp[i])
-	}
-	fmt.Println(lbs_num, lbs_mnc, lbs_mcc)
+	var flag string = "S168#000000000000008#0f12#0071#LOCA:G;CELL:1,1cc,2,2795,1435,64;GDATA:A,12,160412154800,22.564025,113.242329,5.5,152,900;ALERT:0000;STATUS:89,98;WAY:0$"
+
+	arr_buf = strings.Split(flag, "#")                   //先分割#
+	data_buf = strings.Split(string(arr_buf[4]), ";")    //分割;
+	comand_buf = strings.Split(string(data_buf[0]), ":") //分割;
+
+	fmt.Println(comand_buf[0])
 }
 
 func ParseProtocol(rev_buf string, conn net.Conn) {
 	var err error
-	var arr_buf []string
+	var arr_buf, data_buf, comand_buf []string
 
 	fmt.Println("Receive from client", rev_buf)
-	arr_buf = strings.Split(rev_buf, ",")
-	fmt.Println(arr_buf[0])
-	switch arr_buf[0] {
-	case "BDT01": //BDT01,353456789012345#
+
+	arr_buf = strings.Split(rev_buf, "#")                //先分割#
+	data_buf = strings.Split(string(arr_buf[4]), ";")    //分割;
+	comand_buf = strings.Split(string(data_buf[0]), ":") //分割;
+
+	fmt.Println(comand_buf[0])
+
+	switch comand_buf[0] {
+	case "LOCA": //S168#000000000000008#0f12#0071#LOCA:G;CELL:1,1cc,2,2795,1435,64;GDATA:A,12,160412154800,22.564025,113.242329,5.5,152,900;
+		//ALERT:0000;STATUS:89,98;WAY:0$
 		//parse data
 		imei := string(arr_buf[1])
+		serial_num := string(arr_buf[2])
 		//printf data
-		if version == 0 {
-			fmt.Println("IMEI:", imei)
-		}
+		fmt.Println("imei:", imei)
+		fmt.Println("serial_num:", serial_num)
 		//send data
-		zone, _ := strconv.Atoi(GetZone())
-		buf := fmt.Sprintf("BDS01,%s,%d#", GetTimeStamp(), zone)
+		buf := fmt.Sprintf("S168#%s#%s#0009#ACK^LOCA,", imei, serial_num)
 		fmt.Println(buf)
 		_, err = conn.Write([]byte(buf))
 		break
 	case "BDT02": //BDT02,180528,22.5641N,113.2524E,000.1,061830,323.87,060009080002#
 		//parse data
-		latitude := DeleteTail(string(arr_buf[2]))
-		longtitude := DeleteTail(string(arr_buf[3]))
-		signal, sat_num, bat, mode := ParseStatusData(string(arr_buf[7]))
-		speed := string(arr_buf[4])
-		angle := string(arr_buf[6])
+
 		//printf data
-		if version == 0 {
-			fmt.Println("latitude:", latitude)
-			fmt.Println("longtitude:", longtitude)
-			fmt.Println("speed:", speed)
-			fmt.Println("angle:", angle)
-			fmt.Println(signal, sat_num, bat, mode)
-		}
+
 		//send data
 		buf := fmt.Sprintf("BDS02#")
 		fmt.Println(buf)
@@ -142,16 +132,9 @@ func ParseProtocol(rev_buf string, conn net.Conn) {
 		break
 	case "BDT03": //BDT03,7,460,0,9520|3671|13,9520|3672|12,9520|3673|11,9520|3674|10,9520|3675|9,9520|3676|8,9520|3677|7#
 		//parse data
-		lbs_num, _ := strconv.Atoi(string(arr_buf[1]))
-		lbs_mnc := string(arr_buf[2])
-		lbs_mcc := string(arr_buf[3])
+
 		//printf data
-		if version == 0 {
-			fmt.Println(lbs_num, lbs_mnc, lbs_mcc)
-			for i := 4; i < lbs_num+4; i++ {
-				fmt.Println(arr_buf[i])
-			}
-		}
+
 		//send data
 		buf := fmt.Sprintf("BDS03#")
 		fmt.Println(buf)
@@ -159,11 +142,9 @@ func ParseProtocol(rev_buf string, conn net.Conn) {
 		break
 	case "BDT04": //BDT04,060009080002#
 		//parse data
-		signal, sat_num, bat, mode := ParseStatusData(string(arr_buf[1]))
+
 		//printf data
-		if version == 0 {
-			fmt.Println(signal, sat_num, bat, mode)
-		}
+
 		//send data
 		buf := fmt.Sprintf("BDS04#")
 		fmt.Println(buf)
